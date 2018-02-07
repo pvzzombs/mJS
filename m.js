c6c09ebd445066d0f214fa0324e409026f4afd7d
pvzzombs <31880601+pvzzombs@users.noreply.github.com>
2018-02-07 09:23:32 +0000

    V. 1.3
    * Best use for mobile

--- m.js
+++ m.js
@@ -1,4 +1,5 @@
 //version 1.2
+//Miles J.
 //fixed syntax
 //now supports custom array for storing key values
 var m = {};
@@ -17,7 +18,67 @@ m.getKey = function(x,w) {
 	} else if (w[0].length == (w[1].length+1)) {
 	var i, a = w[1] + " ",
 	b = a.split(""),
+	c = //version 1.3
+//fixed syntax
+//enable handling more keys
+var m = {};
+m.Old = "";
+m.New = "";
+m.Store = ["backspace", "enter", "error"];
+m.getKey = function(x,w) {
+//declare variables
+	var i, a, b, c;
+	if(w){
+	var y = w[0];
+	w[0] = x;
+	w[1] = y;
+	if (w[0] == w[1]){
+		return [m.Store[1], -1];
+	} else if ((w[0].length+1) == w[1].length) {
+	return [m.Store[0], -1];
+	} else if (w[0].length == (w[1].length+1)) {
+	a = w[1] + " ";
+	b = a.split("");
 	c = w[0].split("");
+	for (i = 0; i < c.length; i++) {
+	if (c[i] != b[i]) {
+	return [c[i], i];
+	}else if(c[i] == "\n"){
+		return [m.Store[1], i];
+	}
+	}	
+	return ["space", i - 1];
+	} else {
+	return [m.Store[2], -1];
+	}
+	}else{
+    var y = m.New;
+    m.New = x;
+    m.Old = y;
+    if (w[0] == w[1]){
+    return [m.Store[1], -1];
+    } else if ((m.New.length+1) == m.Old.length) {
+        return [m.Store[0], -1];
+    } else if (m.New.length == (m.Old.length+1)) {
+        a = m.Old + " ";
+        b = a.split("");
+        c = m.New.split("");
+        for (i = 0; i < c.length; i++) {
+            if (c[i] != b[i]) {
+                return [c[i], i];
+            }else if(c[i] == "\n"){
+            return [m.Store[1], i];
+            }
+        }	
+        return ["space", i - 1];
+    } else {
+        return [m.Store[2], -1];
+    }
+    }
+};
+m.appear = function(d, el, ty) {
+    document.getElementById(el)[ty] = m.getKey(d);
+};w[0].split("");
 	for (i = 0; i < c.length; i++) {
 	if (c[i] != b[i]) {
 	return [c[i], i];
