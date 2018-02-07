32824497e9fabcd43c4081d8c8d6be5a9d8a1045
pvzzombs <31880601+pvzzombs@users.noreply.github.com>
2018-02-07 10:01:00 +0000

    V. 1.4
    Ok

--- m.js
+++ m.js
@@ -1,26 +1,5 @@
-//version 1.2
-//Miles J.
-//fixed syntax
-//now supports custom array for storing key values
-var m = {};
-m.Old = "";
-m.New = "";
-m.Store = ["backspace", "enter", "error"];
-m.getKey = function(x,w) {
-//m.New = w[0]
-//m.Old = w[1]
-	if(w){
-	var y = w[0];
-	w[0] = x;
-	w[1] = y;
-	if ((w[0].length+1) == w[1].length) {
-	return [m.Store[0], -1];
-	} else if (w[0].length == (w[1].length+1)) {
-	var i, a = w[1] + " ",
-	b = a.split(""),
-	c = //version 1.3
-//fixed syntax
-//enable handling more keys
+//version 1.4
+//removed bugs and added more support
 var m = {};
 m.Old = "";
 m.New = "";
@@ -43,8 +22,6 @@ m.getKey = function(x,w) {
 	for (i = 0; i < c.length; i++) {
 	if (c[i] != b[i]) {
 	return [c[i], i];
-	}else if(c[i] == "\n"){
-		return [m.Store[1], i];
 	}
 	}	
 	return ["space", i - 1];
@@ -55,7 +32,7 @@ m.getKey = function(x,w) {
     var y = m.New;
     m.New = x;
     m.Old = y;
-    if (w[0] == w[1]){
+    if (m.New == m.Old){
     return [m.Store[1], -1];
     } else if ((m.New.length+1) == m.Old.length) {
         return [m.Store[0], -1];
@@ -63,41 +40,6 @@ m.getKey = function(x,w) {
         a = m.Old + " ";
         b = a.split("");
         c = m.New.split("");
-        for (i = 0; i < c.length; i++) {
-            if (c[i] != b[i]) {
-                return [c[i], i];
-            }else if(c[i] == "\n"){
-            return [m.Store[1], i];
-            }
-        }	
-        return ["space", i - 1];
-    } else {
-        return [m.Store[2], -1];
-    }
-    }
-};
-m.appear = function(d, el, ty) {
-    document.getElementById(el)[ty] = m.getKey(d);
-};w[0].split("");
-	for (i = 0; i < c.length; i++) {
-	if (c[i] != b[i]) {
-	return [c[i], i];
-	}
-	}	
-	return ["space", i - 1];
-	} else {
-	return [m.Store[2], -1];
-	}
-	}else{
-    var y = m.New;
-    m.New = x;
-    m.Old = y;
-    if ((m.New.length+1) == m.Old.length) {
-        return [m.Store[0], -1];
-    } else if (m.New.length == (m.Old.length+1)) {
-        var i, a = m.Old + " ",
-            b = a.split(""),
-            c = m.New.split("");
         for (i = 0; i < c.length; i++) {
             if (c[i] != b[i]) {
                 return [c[i], i];
